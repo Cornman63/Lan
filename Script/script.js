f5b6a2d8aadb54f3dc6a53e85357d0e6f7c2b944
Zanhour03 <mikezogheib@gmail.com>
2021-10-12 15:00:44 +0200

    ..
    ..

---
+++ Script/script.js
@@ -0,0 +1,11 @@
+function required(inputtx) 
+   {
+     if (inputtx.value.length == 0)
+      { 
+         alert("message");  	
+         return false; 
+      }  	
+      return true; 
+    } 
+
+    document.querySelector("#vue > div.centerHorinzontal > div:nth-child(3) > div > div:nth-child(4) > ul:nth-child(1) > li:nth-child(4) > span > a")
