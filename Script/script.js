df6fed645a0b5490c735b6218b802a3125d2877d
Zanhour03 <mikezogheib@gmail.com>
2021-10-13 10:57:05 +0200

    ..

--- Script/script.js
+++ Script/script.js
@@ -1,18 +1,18 @@
-GolbalURL = "http://www.yourserverpathtophpfile.com";
+// GolbalURL = "http://www.yourserverpathtophpfile.com";
 
-var cname = $("[name='cname']").val();
-var cnumber = $("[name='cnumber']").val();
-var cemail = $("[name='cemail']").val();
+// var cname = $("[name='cname']").val();
+// var cnumber = $("[name='cnumber']").val();
+// var cemail = $("[name='cemail']").val();
 
-jQuery.ajax({
- type: "POST",
- url: GolbalURL+"sendemail.php",
+// jQuery.ajax({
+//  type: "POST",
+//  url: GolbalURL+"sendemail.php",
 
- data: "cname="+ cname+"& cnumber="+ cnumber+"& cemail="+ cemail,
- dataType: "html",      
- cache: false,
- success: function(response)
- {
-   alert("Email Sent");
- }
-});
+//  data: "cname="+ cname+"& cnumber="+ cnumber+"& cemail="+ cemail,
+//  dataType: "html",      
+//  cache: false,
+//  success: function(response)
+//  {
+//    alert("Email Sent");
+//  }
+// });
